d0e27d22e81b38b93e94524eeb68809e11a123f8
Tanmay Mahendra Kothale <tanmaymkothale@gmail.com>
2022-01-22 01:02:10 -0700

    makefile added

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -1,8 +1,12 @@
 /*
+ * @file: writer.c
+ * 
+ * @author: Tanmay Mahendra Kothale (tanmay-mk)
  *
+ * @date: January 22, 2022
  */
 
-
+/*	LIBRARY FILES	*/
 #include <stdio.h>
 #include <syslog.h>
 #include <stdlib.h>
@@ -10,20 +14,23 @@
 #include <unistd.h>
 #include <fcntl.h>
 
+/*	MACROS	*/
 #define MAX_ARGUMENTS 		(3)
 #define FILE_PERMISSION	(0644)
 
-#define DEBUG			(0)
-
+/*
+ * @brief: application entry point
+ */
 int main(int argc, char *argv[]) 
 {
  
  int writeptr, file;
  
- int writesize;
+ int writesize;  //to compute number of bytes to be written
  
- char *writefile, *writestr;
+ char *writefile, *writestr;	//to store path of file and name of file
  
+ //open a syslog connection
  openlog("AESD Assignment 2",LOG_PID,LOG_USER);
  
  //checking number of inputs
@@ -39,38 +46,47 @@ int main(int argc, char *argv[])
  	exit (1);
  }
 
- writefile = argv[1];
- writestr = argv[2];
+ writefile = argv[1]; 		//storing path of file
+ writestr = argv[2];		//storing string to be written
  
- writesize = strlen(writestr);
+ writesize = strlen(writestr);	//calculate size of strings in byte
  
  //create a file
  file=creat(writefile, FILE_PERMISSION);
  
+ //check if the file was successfully created
  if (file)
  {
+ 	//write the string to the file
  	writeptr = write(file, writestr, writesize);
+ 	//check whether data was successfully written to the file
  	if (writeptr)
- 	{
+ 	{	
+ 		//if total bytes written does not match the size of 
+ 		//string, log the error
  		if (writeptr != writesize)
  		{
  			syslog(LOG_ERR, "%s file incompletely written.", writefile);
  			exit (1);
  		}
+ 		//else print the success message
  		syslog(LOG_DEBUG, "Writing %s to %s", writestr, writefile);
  	}
+ 	//if data is not successfully written, log the error
  	else
  	{
  		syslog(LOG_ERR, "%s file cannot be written", writefile);
  		exit (1);
  	}
  }
+ //if file is not successfully created, log the error
  else
  {
  	syslog(LOG_ERR, "File %s cannot be created\n", writefile);
  	exit (1);
  }
  
+ //close the syslog connection
  closelog();
  return 0;
 } // end main
